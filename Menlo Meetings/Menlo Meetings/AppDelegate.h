ee6d98d18feced9c8dcc39b399e61a1c67064846
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2015-04-26 13:59:16 -0700

    Initial Commit

---
+++ Menlo Meetings/Menlo Meetings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Menlo Meetings
+//
+//  Created by Akshay Srivatsan on 4/26/15.
+//  Copyright (c) 2015 Akshay Srivatsan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Menlo Meetings/Menlo Meetings/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Menlo Meetings
+//
+//  Created by Akshay Srivatsan on 4/26/15.
+//  Copyright (c) 2015 Akshay Srivatsan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
