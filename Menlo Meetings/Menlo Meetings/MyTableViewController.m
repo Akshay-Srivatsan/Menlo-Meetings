269811f4664d96f9bd257a8963ba985085dccda8
Will Borie <will.borie@gmail.com>
2015-04-26 15:12:03 -0700

    Got Table and Button Working

--- Menlo Meetings/Menlo Meetings/MyTableViewController.m
+++ Menlo Meetings/Menlo Meetings/MyTableViewController.m
@@ -10,17 +10,41 @@
 
 @implementation MyTableViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.reminders = @[@"Hello", @"World"];
+}
+
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.reminders.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myTableCell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = self.reminders[indexPath.row];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"%ld", indexPath.row);
+}
+
+/*-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *identifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
     cell.textLabel.text = @"Hi";
+   // NSLog(@"hi");
     return cell;
-}
+}*/
 
 @end
---
+++ Menlo Meetings/Menlo Meetings/TestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewController.h
+//  Menlo Meetings
+//
+//  Created by William Borie on 4/26/15.
+//  Copyright (c) 2015 Akshay Srivatsan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewController
+
+@end
