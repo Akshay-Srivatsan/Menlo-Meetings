a8ed830e1a3c05a05f860acedb3c1112a494174c
nolanjimenez <ab341@comcast.net>
2015-05-23 09:53:34 -0700

    added a googlemapsviewcontroller
    *i did not actually add the googlemaps api to the program, i just
    started a new class that is linked to a view controller

---
+++ Menlo Meetings/Menlo Meetings/GoogleMapsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GoogleMapsViewController.swift
+//  Menlo Meetings
+//
+//  Created by Aaron Brown on 5/22/15.
+//  Copyright (c) 2015 Akshay Srivatsan. All rights reserved.
+//
+
+import UIKit
+
+class GoogleMapsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      //  var camera = GMSCameraPosition.cameraWithLatitude(-33.868,
+       //     longitude:151.2086, zoom:6)
+      //  var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+        
+       // var marker = GMSMarker()
+     //   marker.position = camera.target
+       // marker.snippet = "Hello World"
+        //marker.appearAnimation = kGMSMarkerAnimationPop
+       // marker.map = mapView
+        
+        //self.view = mapView
+    }
+}
