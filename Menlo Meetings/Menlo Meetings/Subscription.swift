b0165a243ba7339c1073db5bbb369b5a95def2c8
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2015-05-24 16:04:24 -0700

    Added Subscription class.
    It isn’t connected to anything yet.

---
+++ Menlo Meetings/Menlo Meetings/Subscription.swift
@@ -0,0 +1,20 @@
+//
+//  Subscription.swift
+//  Menlo Meetings
+//
+//  Created by Akshay Srivatsan on 5/24/15.
+//  Copyright (c) 2015 Akshay Srivatsan. All rights reserved.
+//
+
+import Foundation
+
+class Subscription {
+    var name: String;
+    var teacher: String;
+    
+    
+    init(name: String, teacher: String) {
+        self.name = name;
+        self.teacher = teacher;
+    }
+}
