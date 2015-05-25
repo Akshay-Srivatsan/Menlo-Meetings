165878db538b4250f2352d72bb50a346af1b44e0
nolanjimenez <ab341@comcast.net>
2015-05-25 00:14:31 -0700

    added an annotation to the map

--- Menlo Meetings/Menlo Meetings/MapViewController.swift
+++ Menlo Meetings/Menlo Meetings/MapViewController.swift
@@ -15,9 +15,15 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var centerLocation = CLLocationCoordinate2DMake(37.453353, -122.191094)
+        
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = centerLocation
+        annotation.title = "Menlo School"
+        
         var mapSpan = MKCoordinateSpanMake(0.01, 0.01)
         var mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
         self.map.setRegion(mapRegion, animated: true)
+        map.addAnnotation(annotation)
     }
     
     override func didReceiveMemoryWarning() {
