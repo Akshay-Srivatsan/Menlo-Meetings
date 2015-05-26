15d8f4f967ca0bac730a156ce6e366acc89be8af
nolanjimenez <ab341@comcast.net>
2015-05-25 20:34:30 -0700

    Added a Search Feature for the Map

--- Menlo Meetings/Menlo Meetings/MapViewController.swift
+++ Menlo Meetings/Menlo Meetings/MapViewController.swift
@@ -9,21 +9,58 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
-    @IBOutlet weak var map: MKMapView!
+class MapViewController: UIViewController, UISearchBarDelegate {
+    
+    var searchController:UISearchController!
+    var annotation:MKAnnotation!
+    var localSearchRequest:MKLocalSearchRequest!
+    var localSearch:MKLocalSearch!
+    var localSearchResponse:MKLocalSearchResponse!
+    var error:NSError!
+    var pointAnnotation:MKPointAnnotation!
+    var pinAnnotationView:MKPinAnnotationView!
+    
+    @IBOutlet var mapView: MKMapView!
+    @IBAction func showSearchBar(sender: AnyObject) {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        presentViewController(searchController, animated: true, completion: nil)
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar){
+        //1
+        searchBar.resignFirstResponder()
+        dismissViewControllerAnimated(true, completion: nil)
+        if self.mapView.annotations.count != 0{
+            annotation = self.mapView.annotations[0] as! MKAnnotation
+            self.mapView.removeAnnotation(annotation)
+        }
+        //2
+        localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.startWithCompletionHandler { (localSearchResponse, error) -> Void in
+            
+            if localSearchResponse == nil{
+                var alert = UIAlertView(title: nil, message: "Place not found", delegate: self, cancelButtonTitle: "Try again")
+                alert.show()
+                return
+            }
+            //3
+            self.pointAnnotation = MKPointAnnotation()
+            self.pointAnnotation.title = searchBar.text
+            self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse.boundingRegion.center.latitude, longitude:     localSearchResponse.boundingRegion.center.longitude)
+            
+            
+            self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+            self.mapView.centerCoordinate = self.pointAnnotation.coordinate
+            self.mapView.addAnnotation(self.pinAnnotationView.annotation)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var centerLocation = CLLocationCoordinate2DMake(37.453353, -122.191094)
-        
-        var annotation = MKPointAnnotation()
-        annotation.coordinate = centerLocation
-        annotation.title = "Menlo School"
-        
-        var mapSpan = MKCoordinateSpanMake(0.01, 0.01)
-        var mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
-        self.map.setRegion(mapRegion, animated: true)
-        map.addAnnotation(annotation)
     }
     
     override func didReceiveMemoryWarning() {
